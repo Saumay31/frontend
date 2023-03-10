57ba824d954c0b4a154763d4577103ccaa36aac4
Saumay31 <maheshwarisaumay@gmail.com>
2023-03-10 21:46:24 +0530

    first commit

--- dom/task-3.js
+++ dom/task-3.js
@@ -1,1 +1,7 @@
-console.log("i love india")
+console.log(document.URL);
+console.log(document.title);
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000'
+
+
