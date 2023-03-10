499495694b4b89a08825c6c4aa9c9c823efacbd5
Saumay31 <maheshwarisaumay@gmail.com>
2023-03-10 22:25:21 +0530

    first commit

--- dom/task-3.js
+++ dom/task-3.js
@@ -1,7 +1,14 @@
-console.log(document.URL);
-console.log(document.title);
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000'
+//console.log(document.URL);
+//console.log(document.title);
+//var headerTitle=document.getElementById('header-title');
+//var header=document.getElementById('main-header');
+//header.style.borderBottom='solid 3px #000'
 
-
+var items=document.getElementsByClassName('list-group-item')
+console.log(items);
+console.log(items[1]);
+items[2].textContent="hello"
+items[2].style.backgroundColor='green';
+for(i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold'
+}
